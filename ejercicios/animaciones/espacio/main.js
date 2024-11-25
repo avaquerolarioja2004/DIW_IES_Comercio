31c7c981ad4913074765144a9f17a6f1dbbf8d5f
Ángel Vaquero Toncheva <angelvaquerolarioja@gmail.com>
2024-11-25 18:10:22 +0100

    linkedin acabado y el cockpit a medias

--- ejercicios/animaciones/espacio/main.js
+++ ejercicios/animaciones/espacio/main.js
@@ -1,6 +1,25 @@
+var animacionActiva = false;
+
 document.getElementById('palanca').addEventListener('click', function() {
-    document.getElementById('espacio').classList.add('hiperespacio');
-    setTimeout(function() {
-        document.getElementById('espacio').classList.remove('hiperespacio');
-    }, 10000);
+    var body = document.body;
+    var elementos = document.querySelectorAll('#espacio > div');
+
+    if (!animacionActiva) {
+        // Inicia la animación
+        animacionActiva = true;
+        body.classList.add('activo');
+        elementos.forEach(function(elemento) {
+            elemento.style.visibility = 'hidden';
+        });
+    } else {
+        // Termina la animación
+        animacionActiva = false;
+        body.classList.add('flash');
+        setTimeout(function() {
+            body.classList.remove('activo', 'flash');
+            elementos.forEach(function(elemento) {
+                elemento.style.visibility = 'visible';
+            });
+        }, 1000);
+    }
 });
