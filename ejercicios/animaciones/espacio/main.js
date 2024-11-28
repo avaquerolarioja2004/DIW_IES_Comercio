0b0cecf8270a6d1a1b11f3badf69b851224bf6d4
Ángel Vaquero Toncheva <angelvaquerolarioja@gmail.com>
2024-11-28 17:46:46 +0100

    espacio y pokemon

--- ejercicios/animaciones/espacio/main.js
+++ ejercicios/animaciones/espacio/main.js
@@ -5,13 +5,11 @@ document.getElementById('palanca').addEventListener('click', function() {
     var espacio = document.getElementById('espacio');
 
     if (!animacionActiva) {
-        // Inicia la animación
         animacionActiva = true;
         body.classList.add('activo');
         espacio.classList.add('activo');
         espacio.style.animation = 'none';
     } else {
-        // Termina la animación
         animacionActiva = false;
         setTimeout(function() {
             body.classList.remove('activo');
